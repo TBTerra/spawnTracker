51c07427223a676b497047d71715f52af00afb53
TBTerra <thmsbarber@rudi.org.uk>
2016-07-27 13:35:08 +0100

    newly found spawns that are within work boundrys are now added to the spawns searched

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
-# spawnTracker 0.0.4(alpha)
+# spawnTracker 0.0.5(alpha)
 ## Probably the most efficient large area, long duration tracker for pokemon go data mining
 
 You will need to provide a spawns.json file from [spawnScanner](https://github.com/TBTerra/spawnScan)
 
 - Should handle around 10k spawns per thread/account, assuming they are evenly spread across the hour
 - Should work on up to 16 threads/accounts. beyond that the number of spawns that can be handled per thread starts to drop, and total number of spawns handled remains constant
-- Currently finds new spawns that where missed in initial scan (spawnScanner is about 98% accurate) but does not yet add them to the spawns searched
+- Finds new spawns that where missed in initial scan (spawnScanner is about 97-98% accurate)
 
-config.json is compatible with spawnScanner, and while 'work' section is not currently used, it will be used to set boundary on where new spawns can be found (to avoid spawnpoints growing like a viral infection)
+config.json is compatible with spawnScanner, the work section is used to confine newly found spawns to within the search boundaries. if no work section is present then no boundaries are set
 
 ###Things to do
-
-- make newly found spawns add to the spawns searched (as long as they are within work boundaries)
-- add some way of analysing or visualising the found data
+- add some way of analysing and visualising the found data
--- track.py
+++ track.py
@@ -22,6 +22,17 @@ Pfound = 0
 with open('config.json') as file:
 	config = json.load(file)
 
+def withinWork(lat, lng):
+	if 'work' not in config:
+		return True
+	for rect in config['work']:
+		if (lat>rect[0]) or (lat<rect[2]):
+			continue
+		if (lng<rect[1]) or (lng>rect[3]):
+			continue
+		return True
+	return False
+
 def get_cellid(lat, long):
 	origin = CellId.from_lat_lng(LatLng.from_degrees(lat, long)).parent(15)
 	walk = [origin.id()]
@@ -75,6 +86,10 @@ def doScan(sLat, sLng, sid, api):
 						secSpawn = (gmSpawn.tm_min*60)+(gmSpawn.tm_sec)
 						hash = '{},{}'.format(secSpawn,wild['spawnpoint_id'])
 						Shash[wild['spawnpoint_id']] = secSpawn
+						if withinWork(wild['latitude'],wild['longitude']):
+							pass
+						else:
+							print 'not in work area'
 						#spawns.insert(SbSearch(secSpawn),{'time':secSpawn, 'sid':wild['spawnpoint_id'], 'lat':wild['latitude'], 'lng':wild['longitude'], 'cell':CellId.from_lat_lng(LatLng.from_degrees(wild['latitude'], wild['longitude'])).to_token()})
 						#if timeDif()>0
 						###do somthing here
@@ -103,7 +118,7 @@ def SbSearch(Slist, T):
 	return first
 	
 def worker(wid,Tthreads):
-	global spawns, Shash
+	global spawns, Shash, going
 	#make own spawn list
 	ownSpawns = []
 	for i in xrange(wid,len(spawns),Tthreads):
@@ -128,7 +143,53 @@ def worker(wid,Tthreads):
 			if timeDif(curSec(),ownSpawns[pos]['time']) < 840:#if we arnt 14mins too late
 				while timeDif(curSec(),ownSpawns[pos]['time']) < 60:#wait for 1 min past the spawn time
 					time.sleep(1)
-				doScan(ownSpawns[pos]['lat'],ownSpawns[pos]['lng'],ownSpawns[pos]['sid'],api)
+				sLat = ownSpawns[pos]['lat']
+				sLng = ownSpawns[pos]['lng']
+				sid = ownSpawns[pos]['sid']
+				api.set_position(sLat,sLng,0)
+				timestamp = "\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000"
+				cellid = get_cellid(sLat, sLng)
+				api.get_map_objects(latitude=f2i(sLat), longitude=f2i(sLng), since_timestamp_ms=timestamp, cell_id=cellid)
+				response_dict = api.call()
+				try:
+					resp = response_dict['responses']
+					map = resp['GET_MAP_OBJECTS']
+					cells = map['map_cells']
+				except KeyError:
+					print ('error getting map data for {}, {}'.format(sLat, sLng))
+					return
+				except TypeError:
+					print ('error getting map data for {}, {}'.format(sLat, sLng))
+					return
+				for cell in cells:
+					curTime = cell['current_timestamp_ms']
+					if 'wild_pokemons' in cell:
+						for wild in cell['wild_pokemons']:
+							if wild['time_till_hidden_ms']>0:
+								timeSpawn = (curTime+(wild['time_till_hidden_ms']))-900000
+								if wild['spawnpoint_id'] == sid:
+									#print 'found poke'
+									pokes.append({'time':timeSpawn, 'sid':wild['spawnpoint_id'], 'lat':wild['latitude'], 'lng':wild['longitude'], 'pid':wild['pokemon_data']['pokemon_id'], 'cell':CellId.from_lat_lng(LatLng.from_degrees(wild['latitude'], wild['longitude'])).to_token()})
+									global Pfound
+									Pfound += 1
+								elif wild['spawnpoint_id'] not in Shash:
+									print 'found new spawn'
+									gmSpawn = time.gmtime(timeSpawn//1000)
+									secSpawn = (gmSpawn.tm_min*60)+(gmSpawn.tm_sec)
+									hash = '{},{}'.format(secSpawn,wild['spawnpoint_id'])
+									Shash[wild['spawnpoint_id']] = secSpawn
+									if withinWork(wild['latitude'],wild['longitude']):
+										spawnLog = {'time':secSpawn, 'sid':wild['spawnpoint_id'], 'lat':wild['latitude'], 'lng':wild['longitude'], 'cell':CellId.from_lat_lng(LatLng.from_degrees(wild['latitude'], wild['longitude'])).to_token()}
+										spawns.append(spawnLog)
+										index = SbSearch(ownSpawns,secSpawn)
+										ownSpawns.insert(index,spawnLog)
+										if pos>index:
+											pos += 1
+										if timeDif(ownSpawns[pos]['time'],secSpawn)>0:#only bother to add it to the found pokes, if it has missed its scan window
+											pokeLog = {'time':timeSpawn, 'sid':wild['spawnpoint_id'], 'lat':wild['latitude'], 'lng':wild['longitude'], 'pid':wild['pokemon_data']['pokemon_id'], 'cell':CellId.from_lat_lng(LatLng.from_degrees(wild['latitude'], wild['longitude'])).to_token()}
+											pokes.append(pokeLog)
+									else:
+										print 'not in work area'
 			else:
 				print 'posibly cant keep up. having to drop searches to catch up'
 			pos = (pos+1) % len(ownSpawns)
@@ -138,7 +199,12 @@ def worker(wid,Tthreads):
 def saver():
 	print 'started saver thread'
 	while True:
-		time.sleep(3600)#wait 1 hour
+		countdown = 3600
+		while countdown > 0:
+			if not going:
+				return
+			countdown -= 1
+			time.sleep(1)#wait 1 hour
 		#tell workers to stop
 		global paused, pokes
 		paused = True
@@ -186,7 +252,7 @@ def main():
 	time.sleep(2)
 	#wait for stop signal
 	while True:
-		command = raw_input('Type stop and enter to stop program and save data\ntype prog to see how much data has been gathered ')
+		command = raw_input('Type stop and enter to stop program and save data\ntype prog to see how much data has been gathered\n')
 		if command == 'stop':
 			going = False
 			break
@@ -194,6 +260,7 @@ def main():
 			print 'currently had {} sightings'.format(Pfound)
 	for t in threads:
 		t.join()
+	saverT.join()
 	print 'tracker stoped, had {} pokemeon sightings'.format(Pfound)
 	#print to file
 	if os.path.isfile('pokes.json'):
